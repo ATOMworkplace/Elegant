1c92ad59d1565523fafc67db89672089e4063980
Charchit_Gangwar <114564628+ATOMworkplace@users.noreply.github.com>
2025-06-10 23:01:38 +0530

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'; // Import the Tailwind CSS Vite plugin
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),          // React plugin
-    tailwindcss(),    // Tailwind CSS plugin
-  ],
-  server: {
-    port: 5174,
-  },
-});
+  plugins: [react()],
+  server: {port : 5174}
+})
