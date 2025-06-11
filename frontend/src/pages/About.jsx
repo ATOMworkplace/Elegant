097dcff87c644343224114054370c30761445eae
Charchit_Gangwar <114564628+ATOMworkplace@users.noreply.github.com>
2025-06-11 13:29:14 +0530

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -26,18 +26,18 @@ const About = () => {
       </div>
 
       <div className='flex flex-col md:flex-row text-sm mb-20'>
-        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
-          <b>Quality Assurance:</b>
-          <p className='text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
-        </div>
-        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
-          <b>Convenience: </b>
-          <p className='text-gray-600'>With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
-        </div>
-        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
-          <b>Exceptional Customer Service:</b>
-          <p className='text-gray-600'>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.</p>
-        </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+              <b>Uncompromising Standards:</b>
+              <p className='text-gray-600'>Every item undergoes rigorous inspection to guarantee it aligns with our high benchmarks for excellence.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+              <b>Effortless Experience:</b>
+              <p className='text-gray-600'>Our intuitive design and streamlined processes make your journey from selection to checkout remarkably simple.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+              <b>Dedicated Support:</b>
+              <p className='text-gray-600'>A committed team is readily available to ensure your questions are answered and your needs are met, every step of the way.</p>
+          </div>
       </div>
 
       <NewsletterBox />
